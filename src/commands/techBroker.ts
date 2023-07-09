52a943663abf8432def226602e9b0486e11c0db9
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2023-07-09 23:03:48 +0300

    Update inara scraping links

--- src/commands/techBroker.ts
+++ src/commands/techBroker.ts
@@ -22,7 +22,7 @@ const TechBroker: Command = {
     await interaction.deferReply()
     const systemName = interaction.options.getString('system') || 'Sol'
     const systemNameWeb = encodeURIComponent(systemName)
-    const url = `https://inara.cz/elite/nearest-stations/?formbrief=1&ps1=${systemNameWeb}&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pi17=2&pi2=1&pa1%5B%5D=26&ps2=&pi8=&pi9=0&pi3=&pi4=0&pi5=0&pi6=0&pi7=0&pi23=0&ps3=`
+    const url = `https://inara.cz/elite/nearest-stations/?formbrief=1&ps1=${systemNameWeb}&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pi17=2&pi2=1&pa1%5B%5D=26&ps2=&pi25=0&pi8=&pi9=0&pi26=0&pi3=&pi4=0&pi5=0&pi7=0&pi23=0&pi6=0&ps3=&pi24=0`
 
     const parsedData = await scrapeInara(url, CELLS_PER_ROW)
 
--- tests/inaraScraper.spec.ts
+++ tests/inaraScraper.spec.ts
@@ -1,19 +1,19 @@
 import { scrapeInara } from '../src/utils'
 
 const NO_RECORDS_URL =
-  'https://inara.cz/nearest-stations/?ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pi17=0&ps2=&pi8=&pi9=0&pa2%5B20%5D=1&pi3=&pi4=0&pi5=0&pi6=0&pi7=0&pi23=0'
+  'https://inara.cz/elite/nearest-stations/?formbrief=1&ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pi17=0&ps2=&pi25=0&pi8=&pi9=0&pa2%5B%5D=20&pi26=0&pi3=&pi4=0&pi5=0&pi7=0&pi23=0&pi6=0&ps3=&pi24=0'
 
 const URLS = {
   factors: {
-    url: 'https://inara.cz/nearest-stations/?ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=3&pi19=2000&pi17=1&pi2=1&pa1%5B18%5D=1&pi8=&pi9=0&pi3=&pi4=0&pi5=0&pi6=0&pi7=0&pi23=0',
+    url: 'https://inara.cz/elite/nearest-stations/?formbrief=1&ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=3&pi19=2000&pi17=1&pi2=1&pa1%5B%5D=18&ps2=&pi25=0&pi8=&pi9=0&pi26=0&pi3=&pi4=0&pi5=0&pi7=0&pi23=0&pi6=0&ps3=&pi24=0',
     cellsPerRow: 7,
   },
   broker: {
-    url: 'https://inara.cz/nearest-stations/?ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pa1%5B26%5D=1&pi8=&pi9=0&pi3=&pi4=0&pi5=0&pi6=0&pi7=0&pi23=0',
+    url: 'https://inara.cz/elite/nearest-stations/?formbrief=1&ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pi17=2&pi2=1&pa1%5B%5D=26&ps2=&pi25=0&pi8=&pi9=0&pi26=0&pi3=&pi4=0&pi5=0&pi7=0&pi23=0&pi6=0&ps3=&pi24=0',
     cellsPerRow: 8,
   },
   trader: {
-    url: 'https://inara.cz/nearest-stations/?ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pa1%5B25%5D=1&pi8=&pi9=0&pi3=&pi4=0&pi5=0&pi6=0&pi7=0&pi23=0',
+    url: 'https://inara.cz/elite/nearest-stations/?formbrief=1&ps1=sol&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pi17=2&pi2=1&pa1%5B%5D=25&ps2=&pi25=0&pi8=&pi9=0&pi26=0&pi3=&pi4=0&pi5=0&pi7=0&pi23=0&pi6=0&ps3=&pi24=0',
     cellsPerRow: 8,
   },
 }
