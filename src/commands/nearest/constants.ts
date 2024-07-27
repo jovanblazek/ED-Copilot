e042db1f67efd93ab42050ceb4a4c7330f16eb05
Jovan Blažek <36740941+jovanblazek@users.noreply.github.com>
2024-07-27 18:50:38 +0200

    feat: Group trader, broker & factors command under nearest command (#25)

---
+++ src/commands/nearest/constants.ts
@@ -0,0 +1,3 @@
+export const PARAMS = {
+  system: 'system',
+}
---
+++ src/commands/nearest/techBroker.ts
@@ -0,0 +1,36 @@
+import { chunk } from 'lodash'
+import { SystemNotFoundError } from '../../classes'
+import { usePagination } from '../../embeds'
+import L from '../../i18n/i18n-node'
+import { generateInaraEmbed, ScrapedInaraData, scrapeInara } from '../../utils'
+import { CommandHandler } from '../types'
+import { PARAMS } from './constants'
+
+const CELLS_PER_ROW = 8
+const PAGE_COUNT = 3
+const ROWS_PER_PAGE = 5
+
+export const nearestTechBrokerHandler: CommandHandler = async ({
+  interaction,
+  context: { locale },
+}) => {
+  await interaction.deferReply()
+  const systemName = interaction.options.getString(PARAMS.system) || 'Sol'
+  const systemNameWeb = encodeURIComponent(systemName)
+  const url = `https://inara.cz/elite/nearest-stations/?formbrief=1&ps1=${systemNameWeb}&pi13=&pi14=0&pi15=0&pi16=&pi1=0&pi18=0&pi19=2000&pi17=2&pi2=1&pa1%5B%5D=26&ps2=&pi25=0&pi8=&pi9=0&pi26=0&pi3=&pi4=0&pi5=0&pi7=0&pi23=0&pi6=0&ps3=&pi24=0`
+
+  const parsedData = await scrapeInara(url, CELLS_PER_ROW)
+
+  if (parsedData.length === 0) {
+    throw new SystemNotFoundError({ locale, systemName })
+  }
+
+  const chunks: ScrapedInaraData[][] = chunk(parsedData, ROWS_PER_PAGE)
+  const pages = chunks.slice(0, PAGE_COUNT)
+
+  await usePagination({
+    interaction,
+    locale,
+    embeds: pages.map((page) => generateInaraEmbed(url, page, L[locale].techBroker.title())),
+  })
+}
